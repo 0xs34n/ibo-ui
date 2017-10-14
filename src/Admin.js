825d618bda324bde5eadeebc9eefe534f4d57c2d
Sean Han <seanhan94@gmail.com>
2017-10-14 18:53:54 -0400

    works

--- src/Admin.js
+++ src/Admin.js
@@ -1,74 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ClaimTitle from "./ClaimTitle.js";
 import ClaimCard from "./ClaimCard/ClaimCard.js";
-import Button from "antd/lib/button"
+import Modal from "antd/lib/modal";
+import Button from "antd/lib/button";
+import InputNumber from "antd/lib/input-number";
+import Input from "antd/lib/input";
 
 class Admin extends Component {
-  state = { showModal: false }
+  state = { showModal: false };
 
   showModal = () => {
     this.setState({
-      showModal: true,
+      showModal: true
     });
-  }
+  };
 
-  handleOk = (e) => {
+  handleCreate = e => {
     this.setState({
-      showModal: false,
+      showModal: false
     });
-  }
+  };
 
-  handleCancel = (e) => {
+  handleCancel = e => {
     this.setState({
-      showModal: false,
+      showModal: false
     });
-  }
+  };
 
   render() {
     return (
       <div>
         <ClaimTitle />
-        <Button type="primary" style={{display: "inline-block", marginRight: "50px"}}> Create Bounty </Button>
-        <div style={{
-          display: "flex",
-          justifyContent: "spaceAround",
-          flexWrap: "wrap",
-          flexDirection: "row",
-          width: "80%",
-          marginRight: "10%",
-          marginLeft: "10%"
-        }}>
+        <Button
+          onClick={this.showModal}
+          type="primary"
+          style={{ display: "inline-block", marginRight: "50px" }}
+        >
+          {" "}
+          Create Bounty{" "}
+        </Button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "spaceAround",
+            flexWrap: "wrap",
+            flexDirection: "row",
+            width: "80%",
+            marginRight: "10%",
+            marginLeft: "10%"
+          }}
+        >
           {this.props.bounties.map((bounty, index) => {
-            if(bounty.upload !== "") {
+            if (bounty.upload !== "") {
               return (
                 <ClaimCard
                   key={index}
                   title={bounty.title}
                   upload={bounty.upload}
-                  acceptClaim={() => this.props.acceptClaim(index, bounty.claimID)}
+                  acceptClaim={() =>
+                    this.props.acceptClaim(index, bounty.claimID)}
                   rejectClaim={() => this.props.rejectClaim(index)}
                 />
-              )
+              );
             } else {
               return null;
             }
-            })}
+          })}
         </div>
         <Modal
-          title="Basic Modal"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          okText="CREATE"
-          cancelText="CANCEL"
+          title="Create A New Bounty"
+          visible={this.state.showModal}
+          onOk={this.handleCreate}
+          okText="Create"
+          cancelText="Cancel"
           onCancel={this.handleCancel}
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          <Input placeholder="Title" />
+          <Input placeholder="Icon" />
+          <Input placeholder="Details" />
+          <InputNumber placeholder="Reward" />
         </Modal>
       </div>
     );
   }
 }
+
+/*
+      title,
+      icon,
+      details,
+      reward,
+*/
 
 export default Admin;
